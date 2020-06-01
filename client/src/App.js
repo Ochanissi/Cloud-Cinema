a54fb0e62155f987d84e94e0ff750f43a2216a4b
Mirel Home <mirelalex2094@gmail.com>
2020-06-01 13:19:37 +0300

    added spinner to suspense and created error boundary component and impletemented it

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.scss';
 
+import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+
 import ScrollToTop from './components/scroll-to-top/scroll-to-top.component';
 import Navbar from './components/navbar/navbar.component';
 import Footer from './components/footer/footer.component';
-import HomePage from './pages/home-page/home-page.component';
-import Profile from './pages/profile/profile.component';
-import SignIn from './pages/sign-in/sign-in.component';
-import SignUp from './pages/sign-up/sign-up.component';
-import ItemDetails from './pages/item-details/item-details.component';
-import Discover from './pages/discover/discover.component';
-import DiscoverItem from './pages/discover-item/discover-item.component';
+// import HomePage from './pages/home-page/home-page.component';
+// import Profile from './pages/profile/profile.component';
+// import SignIn from './pages/sign-in/sign-in.component';
+// import SignUp from './pages/sign-up/sign-up.component';
+// import ItemDetails from './pages/item-details/item-details.component';
+// import Discover from './pages/discover/discover.component';
+// import DiscoverItem from './pages/discover-item/discover-item.component';
 
 import {
   getUserWatchedStartAsync,
@@ -21,6 +24,18 @@ import {
 } from './redux/user/user.actions';
 
 // import { setCurrentUser } from './redux/user/user.actions';
+
+const HomePage = lazy(() => import('./pages/home-page/home-page.component'));
+const Profile = lazy(() => import('./pages/profile/profile.component'));
+const SignIn = lazy(() => import('./pages/sign-in/sign-in.component'));
+const SignUp = lazy(() => import('./pages/sign-up/sign-up.component'));
+const ItemDetails = lazy(() =>
+  import('./pages/item-details/item-details.component')
+);
+const Discover = lazy(() => import('./pages/discover/discover.component'));
+const DiscoverItem = lazy(() =>
+  import('./pages/discover-item/discover-item.component')
+);
 
 class App extends React.Component {
   render() {
@@ -42,35 +57,51 @@ class App extends React.Component {
         <ScrollToTop>
           <Navbar />
           <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route
-              exact
-              path='/profile'
-              render={() =>
-                currentUser ? <Profile /> : <Redirect to='/auth/sign-in' />
-              }
-            />
-            <Route
-              exact
-              path='/profile/settings'
-              render={() =>
-                currentUser ? <Profile /> : <Redirect to='/auth/sign-in' />
-              }
-            />
-            <Route
-              exact
-              path='/auth/sign-in'
-              render={() => (currentUser ? <Redirect to='/' /> : <SignIn />)}
-            />
-            <Route
-              exact
-              path='/auth/sign-up'
-              render={() => (currentUser ? <Redirect to='/' /> : <SignUp />)}
-            />
-            <Route exact path='/details/:type/:id' component={ItemDetails} />
-            <Route exact path='/discover' component={Discover} />
-            <Route exact path='/discover/:type/:id' component={DiscoverItem} />
-            <Route exact path='/search/:id' component={DiscoverItem} />
+            <ErrorBoundary>
+              <Suspense fallback={<Spinner />}>
+                <Route
+                  exact
+                  path='/profile'
+                  render={() =>
+                    currentUser ? <Profile /> : <Redirect to='/auth/sign-in' />
+                  }
+                />
+                <Route
+                  exact
+                  path='/profile/settings'
+                  render={() =>
+                    currentUser ? <Profile /> : <Redirect to='/auth/sign-in' />
+                  }
+                />
+                <Route
+                  exact
+                  path='/auth/sign-in'
+                  render={() =>
+                    currentUser ? <Redirect to='/' /> : <SignIn />
+                  }
+                />
+                <Route
+                  exact
+                  path='/auth/sign-up'
+                  render={() =>
+                    currentUser ? <Redirect to='/' /> : <SignUp />
+                  }
+                />
+                <Route
+                  exact
+                  path='/details/:type/:id'
+                  component={ItemDetails}
+                />
+                <Route exact path='/discover' component={Discover} />
+                <Route
+                  exact
+                  path='/discover/:type/:id'
+                  component={DiscoverItem}
+                />
+                <Route exact path='/search/:id' component={DiscoverItem} />
+                <Route path='/' component={HomePage} />
+              </Suspense>
+            </ErrorBoundary>
           </Switch>
           <Footer />
         </ScrollToTop>
--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 
-import './with-spinner.styles.scss';
+import Spinner from '../spinner/spinner.component';
 
-const WithSpinner = (WrappedComponent) => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <div className='spinner-overlay'>
-        <div className='spinner' />
-      </div>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
-  };
-  return Spinner;
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 };
 
 export default WithSpinner;
